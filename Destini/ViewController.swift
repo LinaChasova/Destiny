a3bedcb53856983c19c236f33c36244cc4529627
LinaChasova <a.chasova@innopolis.ru>
2018-12-17 13:04:40 +0300

    finished all the story timeline

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -35,17 +35,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyTextView: UILabel!
     
     // TODO Step 5: Initialise instance variables here
-    
+    var story_num = 1
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
-        
-        
+       
         storyTextView.text = story1
         topButton.setTitle(answer1a, for: .normal)
         bottomButton.setTitle(answer1b, for: .normal)
@@ -54,10 +50,44 @@ class ViewController: UIViewController {
     
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
-    
-        // TODO Step 4: Write an IF-Statement to update the views
+        
+        if story_num == 1 {
+            if sender.tag == 1 {
+                story_num = 3
                 
-        // TODO Step 6: Modify the IF-Statement to complete the story
+                storyTextView.text = story3
+                topButton.setTitle(answer3a, for: .normal)
+                bottomButton.setTitle(answer3b, for: .normal)
+            } else {
+                story_num = 2
+                
+                storyTextView.text = story2
+                topButton.setTitle(answer2a, for: .normal)
+                bottomButton.setTitle(answer2b, for: .normal)
+            }
+        } else if story_num == 2 {
+            if sender.tag == 1 {
+                story_num = 3
+                
+                storyTextView.text = story3
+                topButton.setTitle(answer3a, for: .normal)
+                bottomButton.setTitle(answer3b, for: .normal)
+            } else {
+                story_num = 4
+                
+                storyTextView.text = story4
+            }
+        } else if story_num == 3 {
+            if sender.tag == 1 {
+                story_num = 6
+                
+                storyTextView.text = story6
+            } else {
+                story_num = 5
+                
+                storyTextView.text = story5
+            }
+        }
         
     
     }
